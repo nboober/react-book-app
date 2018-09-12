1f59117763b3be9efa2866b9f149cb1bd5b03ee0
Nick Boober <nboober@terpmail.umd.edu>
2018-09-12 18:48:39 -0400

    doc: added a componentDidMount function in app.js to pull all the books from BooksAPi and created a state to hold them all. Removed the default books from app.js so now only the shelves remain. Updated book.js rendered info to be contained in an li tag

--- src/Book.js
+++ src/Book.js
@@ -4,6 +4,7 @@ import * as BooksAPI from './BooksAPI'
 export class Book extends React.Component{
   render() {
     return(
+    <li>
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
@@ -20,6 +21,7 @@ export class Book extends React.Component{
         <div className="book-title">To Kill a Mockingbird</div>
         <div className="book-authors">Harper Lee</div>
       </div>
+    </li>
     )
   }
 }
