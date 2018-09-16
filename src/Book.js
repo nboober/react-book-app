ebed87e272d37f978c710fa4c621673bdce7683a
Nick Boober <nboober@terpmail.umd.edu>
2018-09-16 13:27:10 -0400

    doc: Ran into an error where if a book didn't have an image it would create an error and break everything. Resolved the issue with a ternary operator. Now if the searched book doesn't have a cover image, author, or title it will say that is not available instead of producing the error

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ export class Book extends React.Component{
       return  <li key={i.title + id}>
                   <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${i.imageLinks.smallThumbnail})` }}></div>
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${i.imageLinks && i.imageLinks.thumbnail ? i.imageLinks.thumbnail : "No Image Available"})` }}></div>
                       <div className="book-shelf-changer">
                         <select>
                           <option value="move" disabled>Move to...</option>
@@ -20,8 +20,8 @@ export class Book extends React.Component{
                         </select>
                       </div>
                     </div>
-                    <div className="book-title">{i.title}</div>
-                    <div className="book-authors">{i.author}</div>
+                    <div className="book-title">{i.title ? i.title : "No title available"}</div>
+                    <div className="book-authors">{i.authors ? i.authors : "No Author available"}</div>
                   </div>
                 </li>
               });
