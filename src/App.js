da5c23d78a53fc62685ff9536efba18cb299dcce
Nick Boober <nboober@terpmail.umd.edu>
2018-09-17 09:16:12 -0400

    doc: created a bookshelf component to hold the different functions needed for bookshelf changing capabiliities

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import { Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Search } from './searchPage'
+import { BookShelf } from './bookShelf'
 
 class BooksApp extends React.Component {
 
@@ -11,42 +12,12 @@ class BooksApp extends React.Component {
     return(
   <div className="app">
     <Route exact path="/" render={()=>(
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-
-                </ol>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search" >Add a book></Link>
-        </div>
+    <div>
+      <BookShelf/>
+      <div className="open-search">
+        <Link to="/search" >Add a book></Link>
       </div>
+    </div>
     )}/>
         <Route path="/search" render={()=>(
           <Search />
---
+++ src/bookShelf.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export class BookShelf extends React.Component{
+
+  render() {
+
+    return(
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
